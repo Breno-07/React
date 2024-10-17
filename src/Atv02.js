cafd3b671e76ea06c3a5676ce6ce0805c046c2ef
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-10-17 10:31:17 -0300

    Add files via upload

---
+++ src/Atv02.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Atv02() {
+    return (
+        <>
+            <h1>Atividade 02</h1>
+            <Link to="/">Voltar para a Home</Link>
+        </>
+    );
+}
+
+export default Atv02;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Letreiro from './Letreiro';
-import Relogio from './Relogio';
+import MinhasRotas from './Minhasrotas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Relogio/>
-    <Letreiro/>
+    <MinhasRotas/>
   </>
 );
