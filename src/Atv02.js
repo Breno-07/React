cff9fb2400317fb6b732d1e636ec1f70aa8db9bb
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-10-17 11:48:06 -0300

    Add files via upload

--- src/Atv02.js
+++ src/Atv02.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ContadorHM from "./ContadorHM"; 
 
-function Atv02() {
+const Atv02 = () => {
     return (
-        <>
+        <div>
             <h1>Atividade 02</h1>
+            <ContadorHM />  
             <Link to="/">Voltar para a Home</Link>
-        </>
+        </div>
     );
 }
 
