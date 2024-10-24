4ddcb324d0b88c2d35a6d12cfe8b24fc19f821ed
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-10-24 11:16:27 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ContadorHM from './ContadorHM'; 
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Contador de Homens e Mulheres</h1>
-        <ContadorHM />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
