ad69a2c409dc62e947f584bb7720c9bc5036c44c
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-10-24 11:16:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
-import MinhasRotas from './Minhasrotas'; 
+import MinhasRotas from './Minhasrotas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <MinhasRotas /> 
-  </BrowserRouter>
+  <>
+    <MinhasRotas/>
+  </>
 );
