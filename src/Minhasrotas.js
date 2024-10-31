8d3d00e105c3a981cf352a368d1f21ed9551ac0f
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-10-31 10:49:03 -0300

    Update Minhasrotas.js

--- src/Minhasrotas.js
+++ src/Minhasrotas.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Atv01 from "./Atv01";
 import Atv02 from "./Atv02";
+import App from './aula4/App';
 
 export default function MinhasRotas() {
     return (
@@ -10,6 +11,8 @@ export default function MinhasRotas() {
                 <Route path="/" element={<Home />} /> 
                 <Route path="/atv01" element={<Atv01 />} /> 
                 <Route path="/atv02" element={<Atv02 />} /> 
+                <Route path="/aula4" element={<App />} />
+
             </Routes>
         </BrowserRouter>
     );
