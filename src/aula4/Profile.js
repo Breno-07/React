46ad382885d8ab645b010209ece88af109b9052c
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-10-31 10:49:48 -0300

    Add files via upload

---
+++ src/aula4/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div>
+            <img src="https://i.imgur.com/7vQD0fPs.jpg" alt="Perfil" />
+            <p>Este é o perfil de um usuário.</p>
+        </div>
+    );
+}
