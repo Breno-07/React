959c447b81f44b102d47cf53545a3c40c3674e5f
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-10-31 11:33:46 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
                 <ul>
                     <li><Link to="/atv01">Atividade 1</Link></li>
                     <li><Link to="/atv02">Atividade 2</Link></li>
+                    <li><Link to="/atv03">Atividade 3</Link></li>
+
                 </ul>
             </nav>
         </div>
