9ac73caae182b9399a37f2dd65c24ca7ad145518
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-11-13 15:19:29 -0300

    Add files via upload

---
+++ React-main/src/Atv03.js
@@ -0,0 +1,21 @@
+import Gallery from './Components/Gallery.js';
+import TodoList from './Components/TodoList.js';
+import PackingList from './Components/PackingList.js';
+import { Link } from "react-router-dom";
+import './Atv03.css';
+import TeaGathering from './Components/TeaGathering.js';
+
+export default function Atv03() {
+    return (
+        <>
+            <h1>Atividade 03</h1>
+      
+            <Gallery />
+            <TodoList />
+            <PackingList />
+            <TeaGathering />
+
+            <Link to="/">&#129144; retornar à página inicial</Link>
+        </>
+    );
+}
