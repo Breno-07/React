a0298341820c7d29ea8ffffdae2a41ec46d77f9b
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-11-21 10:50:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import MinhasRotas from "./minhasRotas";
+import React from 'react';
+import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <MinhasRotas />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
