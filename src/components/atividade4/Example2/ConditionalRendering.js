a4e9a8c30d72dc166cac97b148f951fe1cd97301
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-11-23 15:18:36 -0300

    Delete src/components directory

--- src/components/atividade4/Example2/ConditionalRendering.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ConditionalRendering({ isVisible }) {
-    return <p>{isVisible ? "O componente está visível!" : "O componente está oculto."}</p>;
-}
