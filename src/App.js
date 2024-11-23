18fb7ecfebb3c3945c28ab9f97fb64299c9c792c
Breno Diana <128856150+Breno-07@users.noreply.github.com>
2024-11-23 16:36:06 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Letreiro from "./components/letreiro";
 import Relogio from "./components/relogio";
 import Contador from "./components/contador";
 import Atividade4 from "./components/atividade4/Atividade4"; 
+import Atividade5 from "./components/atividade5/Atividade5";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/atividade2" element={<Relogio />} />
         <Route path="/atividade3" element={<Contador />} />
         <Route path="/atividade4" element={<Atividade4 />} />
+        <Route path="/atividade5" element={<Atividade5 />} />
       </Routes>
     </Router>
   );
